55f2841b9ec1ef5f1027daea5a9fbc6c81574dfa
roselight <nur_412@windowslive.com>
2019-02-20 11:32:16 +0300

    Rota, Zigzag ve Hill Şifreleme

---
+++ kript3/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace kript3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRota = new System.Windows.Forms.TextBox();
+            this.btnRota = new System.Windows.Forms.Button();
+            this.txtAnahtar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnZigzag = new System.Windows.Forms.Button();
+            this.txtZigzag = new System.Windows.Forms.TextBox();
+            this.txtZigAnhtr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblHill = new System.Windows.Forms.Label();
+            this.txtHil = new System.Windows.Forms.TextBox();
+            this.btnhill = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRota
+            // 
+            this.txtRota.Location = new System.Drawing.Point(19, 38);
+            this.txtRota.Name = "txtRota";
+            this.txtRota.Size = new System.Drawing.Size(212, 22);
+            this.txtRota.TabIndex = 0;
+            // 
+            // btnRota
+            // 
+            this.btnRota.Location = new System.Drawing.Point(19, 92);
+            this.btnRota.Name = "btnRota";
+            this.btnRota.Size = new System.Drawing.Size(87, 31);
+            this.btnRota.TabIndex = 1;
+            this.btnRota.Text = "Çalıştır";
+            this.btnRota.UseVisualStyleBackColor = true;
+            this.btnRota.Click += new System.EventHandler(this.btnRota_Click);
+            // 
+            // txtAnahtar
+            // 
+            this.txtAnahtar.Location = new System.Drawing.Point(165, 66);
+            this.txtAnahtar.Name = "txtAnahtar";
+            this.txtAnahtar.Size = new System.Drawing.Size(66, 22);
+            this.txtAnahtar.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Şifreli Metin";
+            // 
+            // btnZigzag
+            // 
+            this.btnZigzag.Location = new System.Drawing.Point(20, 108);
+            this.btnZigzag.Name = "btnZigzag";
+            this.btnZigzag.Size = new System.Drawing.Size(77, 29);
+            this.btnZigzag.TabIndex = 4;
+            this.btnZigzag.Text = "Çalıştır";
+            this.btnZigzag.UseVisualStyleBackColor = true;
+            this.btnZigzag.Click += new System.EventHandler(this.btnZigzag_Click);
+            // 
+            // txtZigzag
+            // 
+            this.txtZigzag.Location = new System.Drawing.Point(20, 29);
+            this.txtZigzag.Name = "txtZigzag";
+            this.txtZigzag.Size = new System.Drawing.Size(173, 22);
+            this.txtZigzag.TabIndex = 5;
+            // 
+            // txtZigAnhtr
+            // 
+            this.txtZigAnhtr.Location = new System.Drawing.Point(137, 74);
+            this.txtZigAnhtr.Name = "txtZigAnhtr";
+            this.txtZigAnhtr.Size = new System.Drawing.Size(56, 22);
+            this.txtZigAnhtr.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Şifreli Metin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Çözülen Metin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Metin Giriniz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Anahtar Giriniz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Çözülen Metin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Metin Giriniz";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Anahtar Giriniz";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtRota);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.btnRota);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtAnahtar);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(29, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(264, 209);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnZigzag);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtZigzag);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtZigAnhtr);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(29, 279);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(257, 217);
+            this.panel2.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(127, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "ROTA ŞİFRELEME";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(29, 256);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 17);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "ZİGZAG ŞİFRELEME";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(416, 44);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(86, 17);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Metin Giriniz";
+            // 
+            // lblHill
+            // 
+            this.lblHill.AutoSize = true;
+            this.lblHill.Location = new System.Drawing.Point(544, 95);
+            this.lblHill.Name = "lblHill";
+            this.lblHill.Size = new System.Drawing.Size(43, 17);
+            this.lblHill.TabIndex = 14;
+            this.lblHill.Text = "Şifreli";
+            // 
+            // txtHil
+            // 
+            this.txtHil.Location = new System.Drawing.Point(419, 64);
+            this.txtHil.Name = "txtHil";
+            this.txtHil.Size = new System.Drawing.Size(212, 22);
+            this.txtHil.TabIndex = 14;
+            this.txtHil.Text = "kelime";
+            // 
+            // btnhill
+            // 
+            this.btnhill.Location = new System.Drawing.Point(415, 118);
+            this.btnhill.Name = "btnhill";
+            this.btnhill.Size = new System.Drawing.Size(87, 31);
+            this.btnhill.TabIndex = 14;
+            this.btnhill.Text = "Çalıştır";
+            this.btnhill.UseVisualStyleBackColor = true;
+            this.btnhill.Click += new System.EventHandler(this.btnhill_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(416, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(98, 17);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "HILL şifreleme";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(544, 118);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(59, 17);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "Çözülen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 543);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btnhill);
+            this.Controls.Add(this.txtHil);
+            this.Controls.Add(this.lblHill);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "ss";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRota;
+        private System.Windows.Forms.Button btnRota;
+        private System.Windows.Forms.TextBox txtAnahtar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnZigzag;
+        private System.Windows.Forms.TextBox txtZigzag;
+        private System.Windows.Forms.TextBox txtZigAnhtr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblHill;
+        private System.Windows.Forms.TextBox txtHil;
+        private System.Windows.Forms.Button btnhill;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ kript3/Form1.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kript3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRota_Click(object sender, EventArgs e)
+        {
+            string metin = txtRota.Text.Trim();
+            metin = metin.Replace(" ", string.Empty);
+            char[] dizi = metin.ToCharArray();
+            int eskiboyut = dizi.Length;
+            int sutun = Convert.ToInt32(txtAnahtar.Text);
+            int satir = (Int32)Math.Ceiling((float)dizi.Length / sutun);
+            Array.Resize(ref dizi, satir * sutun);
+            for (int i = eskiboyut; i < satir * sutun; i++)
+            {
+                dizi[i] = 'X';
+            }
+            char[,] rotamatrisi = new char[satir, sutun];
+
+            for (int i = 0; i < satir; i++)
+            {
+                for (int j = 0; j < sutun; j++)
+                {
+                    rotamatrisi[i, j] = dizi[i * sutun + j];
+                }
+            }
+            string sifrelimetin = "";
+            for (int i = sutun - 1, j = 0; i >= 0; i--, j++)
+            {
+                for (int k = i - 1; k >= j; k--)
+                    sifrelimetin += rotamatrisi[k, j];
+                for (int k = j + 1; k < i; k++)
+                    sifrelimetin += rotamatrisi[j, k];
+                for (int k = j; k < i; k++)
+                    sifrelimetin += rotamatrisi[k, i];
+                for (int k = i - 1; k > j; k--)
+                    sifrelimetin += rotamatrisi[i - 1, k];
+            }
+            label1.Text = sifrelimetin;
+
+
+            /////cozumleme
+            dizi = new char[sifrelimetin.Length];
+            dizi = sifrelimetin.ToCharArray();
+            string cozulenmetin = "";
+            rotamatrisi = new char[sifrelimetin.Length / sutun, sutun];
+            int a = 0;
+            for (int i = sutun - 1, j = 0; i >= 0; i--, j++)
+            {
+                for (int k = i - 1; k >= j; k--)
+                { rotamatrisi[k, j] = dizi[a]; a++; }
+                for (int k = j + 1; k < i; k++)
+                { rotamatrisi[j, k] = dizi[a]; a++; }
+                for (int k = j; k < i; k++)
+                { rotamatrisi[k, i] = dizi[a]; a++; }
+                for (int k = i - 1; k > j; k--)
+                { rotamatrisi[i - 1, k] = dizi[a]; a++; }
+            }
+            for (int i = 0; i < satir; i++)
+            {
+                for (int j = 0; j < sutun; j++)
+                {
+                    cozulenmetin += rotamatrisi[i, j];
+                }
+            }
+            label6.Text = cozulenmetin;
+        }
+
+        private void btnZigzag_Click(object sender, EventArgs e)
+        {
+            string metin = txtZigzag.Text.Trim();
+            metin = metin.Replace(" ", string.Empty);
+            int boyut = Convert.ToInt32(txtZigAnhtr.Text);
+            char[] dizi = metin.ToCharArray();
+            char[,] zigzagMatrisi = new char[boyut, dizi.Length];
+            int x = 0; int y = 0;
+            int sayac = 0;
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < dizi.Length; j++)
+                {
+                    zigzagMatrisi[i, j] = 'X';
+                }
+            }
+            for (int i = 0; i < dizi.Length; i++)
+            {
+
+                if (y % (boyut - 1) == 0 && x != 0)
+                {
+                    sayac = 1;
+
+
+                }
+
+                if (sayac == 1)
+                {
+
+                    zigzagMatrisi[x, y] = dizi[i];
+                    x--;
+                    y++;
+                    if (y % (boyut - 1) == 0 && x == 0)
+                    {
+                        sayac = 0;
+                    }
+                }
+                else
+                {
+
+                    zigzagMatrisi[x, y] = dizi[i]; x++; y++;
+                }
+
+
+
+            }
+            string yenimetin = "";
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < dizi.Length; j++)
+                {
+                    if (zigzagMatrisi[i, j] != 'X')
+                    {
+                        yenimetin += zigzagMatrisi[i, j];
+                    }
+                }
+            }
+
+            label2.Text = yenimetin;
+            dizi = new char[yenimetin.Length];
+            dizi = yenimetin.ToCharArray();
+            zigzagMatrisi = new char[boyut, dizi.Length];
+            x = 0; y = 0;
+            sayac = 0;
+            //    ////////////ÇÖZÜMLEME
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+
+                if (y % (boyut - 1) == 0 && x != 0)
+                {
+                    sayac = 1;
+
+
+                }
+
+                if (sayac == 1)
+                {
+
+                    zigzagMatrisi[x, y] = 'X';
+                    x--;
+                    y++;
+                    if (y % (boyut - 1) == 0 && x == 0)
+                    {
+                        sayac = 0;
+                    }
+                }
+                else
+                {
+
+                    zigzagMatrisi[x, y] = 'X'; x++; y++;
+                }
+
+
+
+            }
+            int a = 0;
+            for (int j = 0; j < boyut; j++)
+            {
+
+
+                for (int i = 0; i < dizi.Length; i++)
+                {
+                    if (zigzagMatrisi[j, i] == 'X')
+                    {
+                        zigzagMatrisi[j, i] = dizi[a];
+                        a++;
+                    }
+                }
+            }
+
+            ///okutma 
+            x = 0;
+            y = 0; sayac = 0;
+            string cozulenmetin = "";
+            for (int i = 0; i < dizi.Length; i++)
+            {
+
+                if (y % (boyut - 1) == 0 && x != 0)
+                {
+                    sayac = 1;
+
+
+                }
+
+                if (sayac == 1)
+                {
+
+                    cozulenmetin += zigzagMatrisi[x, y];
+                    x--;
+                    y++;
+                    if (y % (boyut - 1) == 0 && x == 0)
+                    {
+                        sayac = 0;
+                    }
+                }
+                else
+                {
+
+                    cozulenmetin += zigzagMatrisi[x, y]; x++; y++;
+                }
+
+
+
+            }
+
+
+            label3.Text = cozulenmetin;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnhill_Click(object sender, EventArgs e)
+        {
+            char[] alfabe = { 'q','a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g',
+                                          'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 
+                                          'o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü',
+                                          'v', 'y', 'z' };
+            string metin = txtHil.Text;
+            // int anahtar = Convert.ToInt32(txtHillanahtar.Text);
+            int[,] matris = { { 3, 2, 4 }, { 1, 3, 5 }, { 0, 2, 1 } };
+      
+            char[] chmetin = metin.ToCharArray(); string sonuc = "";
+            int sayı = (int)Math.Ceiling((double)(chmetin.Length / 3));
+            int sayac = 0;
+            int[,] ntmetin = new int[sayı, 3];
+            int[,] sifrelimatris = new int[sayı, 3];
+            for (int i = 0; i < sayı; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    ntmetin[i, j] = Array.IndexOf(alfabe, chmetin[sayac]);
+                    sayac++;
+                }
+
+            }
+
+
+            for (int i = 0; i < sayı; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+
+                        sifrelimatris[i, j] += ntmetin[i, k] * matris[k, j];
+
+                    }
+                    sonuc += alfabe[sifrelimatris[i, j] % 29].ToString();
+                }
+            }
+            lblHill.Text = sonuc;
+        //////ÇÖZME
+          int detA= Determinant(matris);
+            int[,] cofMat=new int[matris.GetLength(0),matris.GetLength(1)];
+            
+
+            for (int i = 0; i < matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris.GetLength(1); j++)
+                {
+                    cofMat[i,j] = (int)Math.Pow(-1, (double)i + j) * Determinant(MinorMatrisi(matris, i, j));
+                }
+            }
+
+            int deger=1;
+            while(Math.Abs(detA)*deger%29!=1)
+            {
+                deger++;
+            }
+            if(detA<0)
+            { deger = deger * (-1); }
+
+            int[,] cofTranspoz = new int[matris.GetLength(0), matris.GetLength(1)];
+            for (int j = 0; j < matris.GetLength(0); j++)
+            {
+                for (int i = 0; i < matris.GetLength(1); i++)
+                {
+                    cofTranspoz[i, j] = cofMat[j, i]; 
+                }
+            }
+         
+            for (int i = 0; i < matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris.GetLength(1); j++)
+                {
+                    if(cofTranspoz[i, j] * deger<0)
+                    {
+
+                        cofTranspoz[i, j] = (cofTranspoz[i, j] * deger) + 29;
+                    }
+                    else cofTranspoz[i, j] = cofTranspoz[i, j] * deger;
+                }
+            }
+            sonuc ="";
+            chmetin = lblHill.Text.ToCharArray();
+            int[,] sifrelimetinindex = new int[sayı, 3]; sayac = 0;
+            for (int i = 0; i < sayı; i++)
+            {
+                for (int j = 0; j <matris.GetLength(0); j++)
+                {
+                    sifrelimetinindex[i, j] = Array.IndexOf(alfabe, chmetin[sayac]);
+                    sayac++;
+                }
+
+            }
+            sifrelimatris = new int[sayı, 3];
+            for (int i = 0; i < sayı; i++)
+            {
+                for (int j = 0; j < matris.GetLength(0); j++)
+                {
+                    for (int k = 0; k <matris.GetLength(1); k++)
+                    {
+
+                        sifrelimatris[i,j] += (sifrelimetinindex[i, k]) * cofTranspoz[k,j];
+
+                    }
+                    sonuc += alfabe[sifrelimatris[i, j]% 29].ToString();
+                }
+            }
+            label13.Text = sonuc;
+
+        }
+        
+        
+        public static int[,] MinorMatrisi(int[,] matris, int x, int y)
+        {
+            int satir = matris.GetLength(0); int sutun = matris.GetLength(1);
+            int[,] yenimatris = new int[satir - 1, sutun - 1];
+            int sayx = 0, sayy = 0;
+            for (int i = 0; i < satir; i++)
+            {
+                for (int j = 0; j < sutun; j++)
+                {
+
+                    if (i == x || j == y) { continue; } // satir=0, sutun=0 kapalı durumu için
+                    else
+                    {
+                        yenimatris[sayx, sayy] = matris[i, j];
+                        sayy++;
+                        if (sayy == satir - 1) { sayy = 0; sayx++; }
+
+                    }
+
+                }
+
+            }
+            //    int cofaktor=Math.Pow(-1,(double)x+y)*
+            return yenimatris;
+        }
+        public static int Determinant(int[,] Matris)
+        {
+            int sonuc = 0;
+            int boyut = int.Parse(Math.Sqrt(Matris.Length).ToString());
+            if (boyut == 1)
+            {
+                return Matris[0, 0];
+            }
+            else if (boyut == 2)
+            {
+                return Matris[0, 0] * Matris[1, 1] - Matris[0, 1] * Matris[1, 0];
+            }
+            for (int i = 0; i < boyut; i++)
+            {
+                int[,] gecicimatris = new int[boyut - 1, boyut - 1];
+
+                gecicimatris = MinorMatrisi(Matris, 0, i);
+
+                sonuc += Matris[0, i] * (int)Math.Pow(-1, (double)i) * Determinant(gecicimatris);
+
+            }
+            return sonuc;
+        }
+        private void btnRSA_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+         
+           
+
+        
+        
+
+    
